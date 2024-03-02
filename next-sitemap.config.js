0eb331cc7b84f074ae04e9a585bde30fc269ff8c
Austin Dunn <austindunn.dev@gmail.com>
2024-03-02 01:52:12 -0800

    update

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://sleepless-development.github.io/',
+    generateRobotsTxt: true,
+    generateIndexSitemap: false,
+    outDir: './out',
+  };
