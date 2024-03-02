50480f52d970fc50a8c22960a9d40b805b91a8ea
Austin Dunn <austindunn.dev@gmail.com>
2024-03-02 02:31:24 -0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,30 +1,74 @@
-import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import React from 'react';
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+
+function useHead() {
+  const { asPath } = useRouter();
+  const { frontMatter, title } = useConfig();
+  const url = `https://sleepless-development.github.io/${asPath}`;
+  const description = frontMatter.description || "Documentation for Sleepless resources for FiveM";
+
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon" href="/static/ox.ico" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={url} />
+    </>
+  );
+}
+
+function useNextSeoProps() {
+  const { asPath } = useRouter();
+  const arr = asPath.replace(/[-_]/g, ' ').split('/');
+  const category = (arr[1][0] !== '#' && arr[1]) || 'Sleepless';
+  const rawTitle = arr[arr.length - 1];
+  const title = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : '%s';
+
+  return {
+    titleTemplate: `${title} - ${
+      rawTitle === category ? 'Documentation' : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
+    }`,
+  };
+}
 
 const config: DocsThemeConfig = {
   logo: (
     <div
       style={{
-        paddingLeft: '65px',
+        paddingLeft: '50px',
         lineHeight: '38px',
-        background: "url('https://i.imgur.com/ZIFuQi2.png') no-repeat left",
-        backgroundSize: '65px',
+        background: "url('https://avatars.githubusercontent.com/u/88127058?s=38') no-repeat left",
+        backgroundSize: '38px',
         fontWeight: 550,
       }}
     >
-      Sleepless Dev
+      Overextended
     </div>
   ),
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: 'https://sleepless-development.github.io/',
   },
   chat: {
-    link: "https://discord.gg/VYV7S9WxTM",
+    link: 'https://discord.gg/aUczAF8gd2',
   },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: 'https://github.com/Sleepless-Development/Sleepless-Development.github.io/blob/main',
   footer: {
-    text: "Sleepless Documentation",
+    text: 'Sleepless',
   },
+  // search: {
+  //   component: <Search />,
+  // },
+  head: useHead,
+  primaryHue: { dark: 200, light: 200 },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+
+  useNextSeoProps: useNextSeoProps,
 };
 
 export default config;
