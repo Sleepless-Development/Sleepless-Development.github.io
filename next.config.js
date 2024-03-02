c4dcb1ac63593caa72a7f1a987249c686dcc7070
Austin Dunn <austindunn.dev@gmail.com>
2024-03-02 02:09:10 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra()
+module.exports = {
+  ...withNextra(),
+  rewrites: null,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
